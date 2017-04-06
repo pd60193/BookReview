fba6f83dcad7342931311405e97ec0f9a28aec11
Anandi Bharwani <anandi.nitd32@gmail.com>
2017-04-07 00:51:22 +0530

    Added functionality to display the list of books found in the search.

--- BookReview/AngularjsViews/controllers/bookSearch.controller.js
+++ BookReview/AngularjsViews/controllers/bookSearch.controller.js
@@ -3,23 +3,43 @@
 
     angular
          .module("BookReview.Controllers")
-          .controller('BookSearchController', ['$q', 'BookService', BookSearchController]);
+          .controller('BookSearchController', ['$q', '$http','BookService', BookSearchController]);
 
 
-    function BookSearchController($q, BookService) {
+    function BookSearchController($q, $http, BookService) {
 
         var self = this;
         self.$q = $q;
         self.greetingMessage = "Hello World";
         self.BookService = BookService;
+        self.bookList = new Array();
 
         self.changeCase = function () {
             self.greetingMessage = self.BookService.changeToLowerCase(self.greetingMessage);
         };
 
         self.alertSearch = function () {
-            self.BookService.alertMessage(self.bookName);
+            alert(self.bookName);
         };
+
+        self.getBookdata = function() {
+            var output = self.BookService.getBookdata(self.bookName);
+            output.then(function succes(response) {
+                for (var i in response.data.items)
+                {
+                    var book = {
+                        name: response.data.items[i].volumeInfo.title,
+                        authors: response.data.items[i].volumeInfo.authors
+                    };
+                    self.bookList.push(book);
+                }
+
+            }, function myError(response) {
+                self.bookData = "no such book";
+            });
+        };
+
+     
     }
 
 })();
--- BookReview/AngularjsViews/services/book.service.js
+++ BookReview/AngularjsViews/services/book.service.js
@@ -5,15 +5,18 @@
         .module('BookReview.Services')
         .service("BookService", ['$q', '$http', BookService]);
 
-    function BookService($q, $http)
-    {
+    function BookService($q, $http) {
         this.changeToLowerCase = function (value) {
             return value.toLowerCase();
         };
+        this.getBookdata = function (bookName) {
+            var obj = {
+                method: "GET",
+                url: "https://www.googleapis.com/books/v1/volumes?q=" + bookName,
+                cache: false
+            }
+            return $http(obj);
+        };
 
-        this.alertMessage = function (value) {
-            alert(value);
-        }
-
-    }
+    };
 })();
